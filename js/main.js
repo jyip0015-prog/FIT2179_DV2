9e8510e5ee639f218ec04b7a46e3a3f0a3db0205
Joshua <jywh2005@gmail.com>
2026-05-19 09:27:11 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,33 @@
+// Load Vega-Lite visualisations
+
+vegaEmbed("#rail-map", "charts/map.json", {
+    actions: false
+})
+    .then(function () {
+        console.log("Railway map loaded successfully.");
+    })
+    .catch(function (error) {
+        console.error("Error loading railway map:", error);
+    });
+
+vegaEmbed("#ridership-line", "charts/line.json", {
+    actions: false
+});
+
+vegaEmbed("#stacked-area", "charts/area.json", {
+    actions: false
+});
+
+vegaEmbed("#heatmap-chart", "charts/heatmap.json", {
+    actions: false
+});
+
+vegaEmbed("#small-multiples-chart", "charts/small_multiples.json");
+
+vegaEmbed("#donut-chart", "charts/donut.json", {
+  actions: false
+});
+
+vegaEmbed("#ranking-chart", "charts/ranking.json", {
+  actions: false
+});
